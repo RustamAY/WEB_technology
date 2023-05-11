874fb1a75189f428f451e0659b5047d9e0df9812
rusyun8 <rustam.yunusov@attolabs.de>
2023-05-11 15:07:26 +0500

    upgrade js and html

---
+++ Fifth_seminar/main.js
@@ -0,0 +1,30 @@
+const paginationButtonsEls = document.querySelectorAll('.pagination');
+for (let i = 0; i < paginationButtonsEls.length; i++) {
+    const button = paginationButtonsEls[i];
+    button.addEventListener('click', async function () {
+        const page = button.getAttribute('data-page');
+        const users = await fetchUsers(page);
+        console.log(users);
+        renderUsers(users);
+    });
+}
+
+async function fetchUsers(page) {
+    const response = await fetch(`https://reqres.in/api/users?page=${page}`);
+    const json = await response.json();
+    return json.data;
+}
+
+function renderUsers(users) {
+    let content = '';
+    for (let i = 0; i < users.length; i++) {
+        const user = users[i];
+        content +=`
+        <div>
+            <img src="${user.avatar}" alt="Аватарка ${user.first_name} ${user.last_name}">
+            <p>${user.first_name} ${user.last_name}</p>
+        </div>
+        `;
+    }
+    document.querySelector('#app').innerHTML = content;
+}
